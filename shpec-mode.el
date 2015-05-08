25d2fd429fc55d499e522c3a41b2336135cb83c9
Adriean Khisbe <adriean.khisbe@live.fr>
2015-05-08 20:17:56 +0200

    autoinsert basic mode documentation

--- shpec-mode.el
+++ shpec-mode.el
@@ -1,5 +1,30 @@
-;; Minor mode for shpec specification
+;;; shpec-mode.el --- Minor mode for shpec specification         -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  Adriean Khisbe
+
+;; Author: AdrieanKhisbe <adriean.khisbe@live.fr>
+;; Keywords: languages, tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
 ;; Building draft
+;;
+
+;;; Code:
+
 
 (defvar shpec-keywords
   `( (,(regexp-opt '( "describe" "it" "end") ; keywords
@@ -22,4 +47,7 @@
 
 ;; §TODO: make it extend sh-mode
 ;; hook to run test
+
+
 (provide 'shpec-mode)
+;;; shpec-mode.el ends here
