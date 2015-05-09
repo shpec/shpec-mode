9c203330c5ed8738b203b62be7cda83f7a786a51
Adriean Khisbe <adriean.khisbe@live.fr>
2015-05-09 10:38:20 +0200

    add more metadata to shpec-mode header

--- shpec-mode.el
+++ shpec-mode.el
@@ -3,7 +3,12 @@
 ;; Copyright (C) 2015  Adriean Khisbe
 
 ;; Author: AdrieanKhisbe <adriean.khisbe@live.fr>
+;; Version: 0.0.1
 ;; Keywords: languages, tools
+;; URL: http://github.com/shpec/shpec-mode
+;; Package-Requires: ()
+
+;; This file is NOT part of GNU Emacs.
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
