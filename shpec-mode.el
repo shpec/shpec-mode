af42bb4fe3f4911d80c0cd171607c3ca1d0bf067
Adriean Khisbe <adriean.khisbe@live.fr>
2015-05-29 22:04:21 +0200

    untabify and add mode description in elisp comment

--- shpec-mode.el
+++ shpec-mode.el
@@ -25,7 +25,7 @@
 
 ;;; Commentary:
 
-;; Building draft
+;; This is a minimal, *in-building* emacs mode for shpec shell specifications.
 ;;
 
 ;;; Code:
@@ -33,14 +33,14 @@
 
 (defvar shpec-keywords
   `( (,(regexp-opt '( "describe" "it" "end") ; keywords
-		   'words) . 'font-lock-function-name-face)
+                   'words) . 'font-lock-function-name-face)
      ("assert" . 'font-lock-type-face) ; improve with arg
      (,(regexp-opt '("stub_command" "unstub_command")
-		   'words) . 'font-lock-function-name-face)
+                   'words) . 'font-lock-function-name-face)
      ;;matchers:
      (,(regexp-opt '("equal" "unequal" "gt" "lt" "match" "no_match"
-		     "present" "blank" "file_present" "file_absent" "symlink" "test")
-		   'words) . 'font-lock-builtin-face)
+                     "present" "blank" "file_present" "file_absent" "symlink" "test")
+                   'words) . 'font-lock-builtin-face)
      )
   "Keyword for shpec specification.")
 
